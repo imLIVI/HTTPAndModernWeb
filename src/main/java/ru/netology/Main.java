e0a15067f08ee6f092179689060f1e1c42761c70
imLIVI <lesly120898@gmail.com>
2023-02-04 22:35:11 +0300

    Implement multithreading

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -4,15 +4,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png",
-                "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html",
-                "/classic.html", "/events.html", "/events.js");
         final var port = 9999;
 
-        Server server = new Server(validPaths);
+        Server server = new Server();
         server.start(port);
-        server.handle();
-        server.end();
+        //server.end();
     }
 }
 
